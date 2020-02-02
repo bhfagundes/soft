c100cefa0bfcc9b08462cc95af81f93f638adb15
Brenner Fagundes <bhfagundes@gmail.com>
2020-02-02 00:54:37 -0200

    autenticacao completa

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -7,20 +7,15 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("/api/users/register", userData)
+    .post("http://localhost:3000/api/register", userData)
     .then(res => history.push("/"))
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+    .catch(err => alert(err));
 };
 
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post("/api/users/login", userData)
+    .post("http://localhost:3000/api/login", userData)
     .then(res => {
       // Save to localStorage
 
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -7,129 +7,128 @@ import { loginUser } from "../../actions/authActions";
 import "./Auth.scss";
 
 class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: "",
-            password: "",
-            errors: {}
-        };
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+  }
+
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
     }
 
-    componentDidMount() {
-        // If logged in and user navigates to Login page, should redirect them to dashboard
-        if (this.props.auth.isAuthenticated) {
-            this.props.history.push("/dashboard");
-        }
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.auth.isAuthenticated) {
-            this.props.history.push("/dashboard");
-        }
+  onChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
 
-        if (nextProps.errors) {
-            this.setState({
-                errors: nextProps.errors
-            });
-        }
-    }
+  onSubmit = e => {
+    e.preventDefault();
 
-    onChange = e => {
-        this.setState({ [e.target.id]: e.target.value });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
     };
 
-    onSubmit = e => {
-        e.preventDefault();
+    this.props.loginUser(userData);
+  };
 
-        const userData = {
-            email: this.state.email,
-            password: this.state.password
-        };
+  fillDemoEmail = () => {
+    this.setState({ email: "test@test.com" });
+  };
 
-        this.props.loginUser(userData);
-    };
+  fillDemoPassword = () => {
+    this.setState({ password: "test123" });
+  };
 
-    fillDemoEmail = () => {
-        this.setState({ email: "test@test.com" });
-    };
+  render() {
+    const { errors } = this.state;
 
-    fillDemoPassword = () => {
-        this.setState({ password: "test123" });
-    };
+    return (
+      <div className="base-wrapper">
+        <div className="auth-header">Sign In</div>
+        <form className="auth-form" noValidate onSubmit={this.onSubmit}>
+          <div className="auth-group">
+            <label>
+              <div className="auth-label">Email address</div>
+              <input
+                onChange={this.onChange}
+                value={this.state.email}
+                error={errors.email}
+                id="email"
+                type="email"
+                className="auth-input"
+              />
+              <div className="auth-error">
+                {errors.email}
+                {errors.emailnotfound}
+              </div>
+            </label>
+          </div>
 
-    render() {
-        const { errors } = this.state;
+          <div className="auth-group">
+            <label>
+              <div className="auth-label">Password</div>
+              <input
+                onChange={this.onChange}
+                value={this.state.password}
+                error={errors.password}
+                id="password"
+                type="password"
+                className="auth-input"
+              />
+              <div className="auth-error">
+                {errors.password}
+                {errors.passwordincorrect}
+              </div>
+            </label>
+          </div>
 
-        return (
-            <div className="base-wrapper">
-                <div className="auth-header">Sign In</div>
-                <form className="auth-form" noValidate onSubmit={this.onSubmit}>
-                    <div className="auth-group">
-                        <label>
-                            <div className="auth-label">Email address</div>
-                            <input
-                                onChange={this.onChange}
-                                value={this.state.email}
-                                error={errors.email}
-                                id="email"
-                                type="email"
-                                className="auth-input"
-                            />
-                            <div className="auth-error">
-                                {errors.email}
-                                {errors.emailnotfound}
-                            </div>
-                        </label>
-                    </div>
-
-                    <div className="auth-group">
-                        <label>
-                            <div className="auth-label">Password</div>
-                            <input
-                                onChange={this.onChange}
-                                value={this.state.password}
-                                error={errors.password}
-                                id="password"
-                                type="password"
-                                className="auth-input"
-                            />
-                            <div className="auth-error">
-                                {errors.password}
-                                {errors.passwordincorrect}
-                            </div>
-                        </label>
-                    </div>
-
-                    <div>
-                        <button type="submit" className="auth-button">
-                            Login
+          <div>
+            <button type="submit" className="auth-button">
+              Login
             </button>
-                    </div>
-                    <div className="bottom-group">
-                        <Link to="/register" className="link">
-                            Sign up
+          </div>
+          <div className="bottom-group">
+            <Link to="/register" className="link">
+              Sign up
             </Link>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 Login.propTypes = {
-    loginUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    errors: PropTypes.object.isRequired
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
+  auth: state.auth,
+  errors: state.errors
 });
 
 export default connect(
-    mapStateToProps,
-    { loginUser }
+  mapStateToProps,
+  { loginUser }
 )(Login);
-
